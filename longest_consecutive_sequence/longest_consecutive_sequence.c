c871236e1f3c14a9b7b5d9ce64bc7bf69a976d63
damonholden <damonh003@gmail.com>
2024-12-18 23:53:12 +0000

    implement index assignment solution for longest consecutive sequence
    New solution works well but has the problem of allocating an array which has a size equal to the range of the lowest and highest number in the provided nums array. This is problematic for when the solution is provided a small-sized array with a large maximum-minimum value range.

--- longest_consecutive_sequence/longest_consecutive_sequence.c
+++ longest_consecutive_sequence/longest_consecutive_sequence.c
@@ -5,21 +5,50 @@ int32_t longestConsecutive(int32_t *nums, int numsSize)
 		return 0;
 	}
 
-	bool sorted_nums[3000000000];
+	int32_t highest_number = nums[0];
+	int32_t lowest_number = nums[0];
+	for (int32_t index = 1; index < numsSize; index++)
+	{
+		if (nums[index] > highest_number)
+		{
+			highest_number = nums[index];
+		}
+
+		else if (nums[index] < lowest_number)
+		{
+			lowest_number = nums[index];
+		}
+	}
+	int32_t offset = highest_number + abs(lowest_number);
+
+	// printf("%d\n", lowest_number);
+	// printf("%d\n", abs(lowest_number));
+	// printf("%d\n", highest_number);
+	// printf("%d\n", offset);
+
+	bool *sorted_nums = calloc(offset + 1, sizeof(bool));
 
 	for (int32_t index = 0; index < numsSize; index++)
 	{
-		sorted_nums[nums[index] + 1000000000] = true;
+		sorted_nums[nums[index] + abs(lowest_number)] = true;
 	}
 
 	int32_t highest_sequence = 0;
 	int32_t current_sequence = 0;
 
-	for (int32_t index = 0; index < numsSize; index++)
+	for (int32_t index = 0; index < offset + 1; index++)
 	{
+		// printf("index %d\n", sorted_nums[index]);
+		// printf("high %d\n", highest_sequence);
+		// printf("low %d\n", current_sequence);
 		if (sorted_nums[index] == true)
 		{
 			current_sequence++;
+
+			if (current_sequence > highest_sequence)
+			{
+				highest_sequence = current_sequence;
+			}
 			continue;
 		}
 
