e783b4a4a79f458dfc9a02b5086f9a40094a1e47
damonholden <damonh003@gmail.com>
2024-12-18 21:46:58 +0000

    format longest_consecutive_sequence.c

--- longest_consecutive_sequence/longest_consecutive_sequence.c
+++ longest_consecutive_sequence/longest_consecutive_sequence.c
@@ -1,29 +1,35 @@
-int32_t longestConsecutive(int32_t* nums, int numsSize) {
-    if (numsSize == 0) {
-        return 0;
-    }
+int32_t longestConsecutive(int32_t *nums, int numsSize)
+{
+	if (numsSize == 0)
+	{
+		return 0;
+	}
 
 	bool sorted_nums[3000000000];
 
-    for (int32_t index = 0; index < numsSize; index++) {
+	for (int32_t index = 0; index < numsSize; index++)
+	{
 		sorted_nums[nums[index] + 1000000000] = true;
 	}
 
-    int32_t highest_sequence = 0;
-    int32_t current_sequence = 0;
-    
-    for (int32_t index = 0; index < numsSize; index++) {
-		if (sorted_nums[index] == true) {
+	int32_t highest_sequence = 0;
+	int32_t current_sequence = 0;
+
+	for (int32_t index = 0; index < numsSize; index++)
+	{
+		if (sorted_nums[index] == true)
+		{
 			current_sequence++;
-			continue; 
+			continue;
 		}
 
-		if (current_sequence > highest_sequence) {
+		if (current_sequence > highest_sequence)
+		{
 			highest_sequence = current_sequence;
 		}
 
 		current_sequence = 0;
-    }
+	}
 
-    return highest_sequence;
+	return highest_sequence;
 }
