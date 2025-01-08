ebb7e0578802c9c8543e6bc0fb36cd4311014f15
damonholden <damonh003@gmail.com>
2025-01-08 14:30:31 +0000

    commit last update of longest consecutive sequence solution

--- longest_consecutive_sequence/longest_consecutive_sequence.c
+++ longest_consecutive_sequence/longest_consecutive_sequence.c
@@ -19,28 +19,40 @@ int32_t longestConsecutive(int32_t *nums, int numsSize)
 			lowest_number = nums[index];
 		}
 	}
-	int32_t offset = highest_number + abs(lowest_number);
+
+	int32_t size_of_map = highest_number + abs(lowest_number);
 
 	// printf("%d\n", lowest_number);
 	// printf("%d\n", abs(lowest_number));
 	// printf("%d\n", highest_number);
-	// printf("%d\n", offset);
+	// printf("%d\n", size_of_map);
 
-	bool *sorted_nums = calloc(offset + 1, sizeof(bool));
+	bool *sorted_nums = calloc(size_of_map + 1, sizeof(bool));
 
 	for (int32_t index = 0; index < numsSize; index++)
 	{
 		sorted_nums[nums[index] + abs(lowest_number)] = true;
 	}
 
-	int32_t highest_sequence = 0;
-	int32_t current_sequence = 0;
-
-	for (int32_t index = 0; index < offset + 1; index++)
+	int32_t highest_sequence = 1;
+	int32_t current_sequence = 1;
+	int32_t next_num = 0;
+	int32_t current_visited_size = 0;
+	int32_t vistited[numsSize];
+	for (int32_t index = 0; index < numsSize; index++)
 	{
 		// printf("index %d\n", sorted_nums[index]);
 		// printf("high %d\n", highest_sequence);
 		// printf("low %d\n", current_sequence);
+		if (nums[index] + abs(lowest_number) == 0) {
+			while (nums[index] + abs(lowest_number) + 1 != size_of_map) {
+				if (sorted_nums[nums[index] + abs(lowest_number) + 1] == true) {
+					current_sequence++
+				}
+			}
+
+		}
+
 		if (sorted_nums[index] == true)
 		{
 			current_sequence++;
